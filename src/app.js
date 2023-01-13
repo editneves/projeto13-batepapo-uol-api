088f8450befd14fb1f4bad3df45657c375d2ab79
edtneves <edianeunb@gmail.com>
2023-01-13 16:25:40 -0300

    banco de dados participantes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,47 @@
-import  express  from "express";
+import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db;
+
+try {
+    await mongoClient.connect()
+    db = mongoClient.db("Batebapo")
+} catch (error) {
+    console.log('Deu erro no app')
+}
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+    try {
+
+        const participante = await db.collection("participants").findOne({ name })
+
+        if (participante) return res.status(409).send("Esse nome já está cadastrado!")
+
+        await db.collection("participants").insertOne({ name })
+
+        res.send("ok")
+
+    } catch (err) {
+        console.log(err)
+        res.status(422).send("O nome não foi salvo!")
+    }
+})
+
+
+
+
+
+
 const PORT = 5000;
-
-
-app.listen(PORT, () => `servidor rodando na porta${PORT}`)
+app.listen(PORT, () => console.log(`servidor rodando na porta${PORT}`))
